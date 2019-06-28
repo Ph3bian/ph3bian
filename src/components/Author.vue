5cecbe7f2fa54748df856c12e16564eaf57bf059
Phebian Chukwurah <phebian@Phebians-MacBook-Pro.local>
2019-06-28 23:42:24 +0100

    first commit

---
+++ src/components/Author.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="author">
+
+		<g-image alt="Author image" class="author__image" src="~/assets/images/me.png" width="180" height="180" blur="5" />
+
+		<h1 v-if="showTitle" class="author__site-title">
+			{{ $static.metaData.siteName }}
+		</h1>
+
+		<p class="author__intro">
+			Phebian is a front end developer, growing, learning and killing it.
+		</p>
+
+		<p class="author__links">
+			<a href="//twitter.com/ph3bian">Follow on Twitter</a>
+			<a href="//github.com/ph3bian">GitHub</a>
+		</p>
+
+	</div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+	props: ['showTitle']
+}
+</script>
+
+<style lang="scss">
+.author {
+	margin: 0 auto;
+	max-width: 500px;
+	text-align: center;
+	padding: calc(var(--space) / 2) 0;
+
+	&__image {
+		border-radius: 100%;
+		width: 90px;
+		height: 90px;
+		margin-bottom: 1em;
+	}
+
+	&__intro {
+		opacity: .8;
+	}
+
+	&__site-title {
+		font-size: 1.5em;
+	}
+
+	&__links {
+		margin-top: -.5em;
+		a {
+			margin: 0 .5em;
+		}
+	}
+}
+</style>
